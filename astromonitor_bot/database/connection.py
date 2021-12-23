da958b23252f28b39526ea6371a56077a126e883
mattia <promat85@gmail.com>
2021-12-23 23:20:09 +0100

    Add new method for deleting user tokens
    Also start testing

--- astromonitor_bot/database/connection.py
+++ astromonitor_bot/database/connection.py
@@ -1,10 +1,12 @@
 import sqlite3
 
+from astromonitor_bot.database.factory import db_factory
+
 
 class _Connection:
 
     def execute(self, query, args=None):
-        with sqlite3.connect('astromonitor.db') as conn:
+        with sqlite3.connect(db_factory()) as conn:
             cursor = conn.cursor()
             if args:
                 return cursor.execute(query, args)
@@ -21,5 +23,7 @@ class _Connection:
     def start(self):
         self._setup_schema()
 
-
+    def count_tokens(self):
+        return self.execute('SELECT COUNT (*) from api_tokens').fetchone()
+        
 connection = _Connection()
---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+
+from astromonitor_bot.database.connection import connection
+from astromonitor_bot.database.factory import db_factory
+
+@pytest.fixture(autouse=True)
+def setup_test_env_fixture(request, scope='session'):
+    connection.start()
+    def drop_db():
+        os.remove(db_factory())
+
+    request.addfinalizer(drop_db)
+    
